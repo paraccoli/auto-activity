4014745a868976b0e7d74d3487d6b106889fb926
Paraccoli <m.mirim1357@gmail.com>
2025-06-11 13:35:12 +0900

    Delete run-auto-pr.sh

--- run-auto-pr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/paraccoli/apps/auto-activity
-source venv/bin/activate
-source .env
-export GITHUB_TOKEN
-export GITHUB_USERNAME
-export GITHUB_REPO
-python3 github-auto-pr.py
-deactivate
